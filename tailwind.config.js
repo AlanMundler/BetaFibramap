7a2406d12b8726c6c24eb6613bd91eb61e51481c
Alan Mundler <alan.mundler@gmail.com>
2024-04-12 12:36:52 -0300

    Fibramap con comentarios, mapa y crud

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [
+    require("flowbite/plugin")
+  ],
+  theme: {},
+  
 };
